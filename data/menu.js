d4a58e1159ed1ada393e78269ce6e43ea8603620
ALI ABDOU <ali.abdoufb@gmail.com>
2025-08-21 23:07:27 +0100

    first commit

---
+++ data/menu.js
@@ -0,0 +1,82 @@
+export const menuList = [
+  {
+    title: "Home",
+    links: [
+      { href: "/", label: "Home 1" },
+      { href: "/home-2", label: "Home 2" },
+      { href: "/home-3", label: "Home 3" },
+      { href: "/home-4", label: "Home 4" },
+      { href: "/home-5", label: "Home 5" },
+      { href: "/home-6", label: "Home 6" },
+      { href: "/home-7", label: "Home 7" },
+      { href: "/home-8", label: "Home 8" },
+      { href: "/home-9", label: "Home 9" },
+      { href: "/home-10", label: "Home 10" },
+    ],
+  },
+  {
+  title: "Formations",
+  links: [
+    { href: "/formations/tronc-commun",    label: "Tronc commun" },
+    { href: "/formations/licence",         label: "Licence" },
+    { href: "/formations/master",          label: "Master" },
+    { href: "/formations/doctorat",        label: "Doctorat" },
+    { href: "/formations/cycle-ingenieur", label: "Cycle ingénieur" },
+  ],
+},
+  {
+    title: "Actualiés",
+    links: [
+      { href: "/event-list-1", label: "Event List 1" },
+      { href: "/event-list-2", label: "Event List 2" },
+      { href: "/events/1", label: "Event Single" },
+      { href: "/event-cart", label: "Event Cart" },
+      { href: "/event-checkout", label: "Event Checkout" },
+    ],
+  },
+  {
+    title: "Blogs",
+    links: [
+      { href: "/blog-list-1", label: "Blog List 1" },
+      { href: "/blog-list-2", label: "Blog List 2" },
+      { href: "/blog-list-3", label: "Blog List 3" },
+      { href: "/blogs/3", label: "Blog Single" },
+    ],
+  },
+  {
+    title: "Pages",
+    links: [
+      {
+        title: "About Us",
+        links: [
+          { href: "/about-1", label: "About 1" },
+          { href: "/about-2", label: "About 2" },
+        ],
+      },
+      {
+        title: "Contact",
+        links: [
+          { href: "/contact-1", label: "Contact 1" },
+          { href: "/contact-2", label: "Contact 2" },
+        ],
+      },
+      {
+        title: "Shop",
+        links: [
+          { href: "/shop-cart", label: "Shop Cart" },
+          { href: "/shop-checkout", label: "Shop Checkout" },
+          { href: "/shop-list", label: "Shop List" },
+          { href: "/shop-order", label: "Shop Order" },
+          { href: "/shop/3", label: "Shop Single" },
+        ],
+      },
+      { href: "/pricing", label: "Membership plans" },
+      { href: "/not-found", label: "404 Page" },
+      { href: "/terms", label: "FAQs" },
+      { href: "/help-center", label: "Help Center" },
+      { href: "/login", label: "Login" },
+      { href: "/signup", label: "Register" },
+      { href: "/ui-elements", label: "UI Elements" },
+    ],
+  },
+];
